0965fc7bf4b660df4f73aa1240f9dd1c7ce8106d
Yaman Umuroglu <maltanar@gmail.com>
2014-11-08 21:26:23 +0100

    remove fixed-size ColdMissSkip sources from toplevel

--- vector-cache-src/VColdMissSkipVectorCache.cpp
+++
@@ -1,5 +0,0 @@
-#ifndef CACHE_IMPLFILE
-#define CACHE_IMPLFILE  "cold-skip/depth-8192/VColdMissSkipVectorCache.cpp"
-#endif
-
-#include CACHE_IMPLFILE
--- vector-cache-src/VColdMissSkipVectorCache.h
+++
@@ -1,5 +0,0 @@
-#ifndef CACHE_HDRFILE
-#define CACHE_HDRFILE  "cold-skip/depth-8192/VColdMissSkipVectorCache.h"
-#endif
-
-#include CACHE_HDRFILE
